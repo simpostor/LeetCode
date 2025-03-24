ad7f0ec5642c6935187871e03f47b7953ca0eee9
Atharva Sankhe <84955573+simpostor@users.noreply.github.com>
2025-03-24 14:58:23 +0530

    Time: 4 ms (58.13%) | Memory: 50.4 MB (5.13%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -2,18 +2,20 @@ class Solution {
     public boolean isPalindrome(String s) {
         int l = 0;
         int r = s.length()-1;
-        while(l<r){
-            while(l<r && !Character.isLetterOrDigit(s.charAt(l))){//while l is not letter or digit
-                l++;
-            }//now l at an index where char is letter or digit
-            while(l<r && !Character.isLetterOrDigit(s.charAt(r))){//while l is not letter or digit
-                r--;
-            }// s at r has letter or digit
-            if(Character.toUpperCase(s.charAt(r)) != Character.toUpperCase(s.charAt(l)))
-                return false;
+        return recursion(s,l,r);
+    }
+    public boolean recursion(String s , int l, int r){
+        while(l<r && !(Character.isLetterOrDigit(s.charAt(l)))){
             l++;
+        }
+        while(l<r && !(Character.isLetterOrDigit(s.charAt(r)))){
             r--;
         }
-        return true;
+        if(l>=r)
+            return true;//base case
+        if(Character.toUpperCase(s.charAt(l)) != Character.toUpperCase(s.charAt(r)))
+            return false;//false case
+        else
+            return recursion(s,l+1,r-1);//move to next depth
     }
 }
