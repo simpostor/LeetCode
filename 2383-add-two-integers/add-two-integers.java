b469106e54971edf3e3b6dd5cfa5bfaba625e356
Atharva Sankhe <84955573+simpostor@users.noreply.github.com>
2025-03-25 15:53:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (22.60%) - LeetSync

--- 2383-add-two-integers/add-two-integers.java
+++ 2383-add-two-integers/add-two-integers.java
@@ -1,5 +1,11 @@
 class Solution {
     public int sum(int num1, int num2) {
-        return num1+num2;
+        while(num2!=0){
+            int xor = num1 ^ num2;
+            int carry = (num1 & num2) <<1;
+            num1 = xor;
+            num2 = carry;
+        }
+        return num1;
     }
 }
