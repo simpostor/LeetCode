2cd503d71f8062a63b507ca7018eb3e03eeb6f6a
Atharva Sankhe <84955573+simpostor@users.noreply.github.com>
2025-04-04 13:05:15 +0530

    Time: 2 ms (19.40%) | Memory: 44.5 MB (39.73%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
@@ -14,44 +14,60 @@
  * }
  */
 class Solution {
-    
-    public TreeNode lcaDeepestLeaves(TreeNode root) {
-        int[] pair = findDeepest(root);  // [first, second]
-        int p = pair[0];
-        int q = pair[1];
-        return lowestCommonAncestor(root, p, q);
+    static class DeepestNodes{
+        int first;
+        int second;
+        DeepestNodes(int first, int second){
+            this.first = first;
+            this.second = second;
+        }
     }
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        //find deepest leaves func
+        //find lca dunction
+        //return node
+        DeepestNodes Pair = findDeepest(root);
+        int p = Pair.first;
+        int q = Pair.second;
+        return lowestCommonAncestor(root, p, q);
 
-    public int[] findDeepest(TreeNode root){
+
+    }
+    public DeepestNodes findDeepest(TreeNode root){
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
         int first = 0;
         int last = 0;
         while(!queue.isEmpty()){
             int size = queue.size();
-            first = ((LinkedList<TreeNode>) queue).peekFirst().val;
-            last = ((LinkedList<TreeNode>) queue).peekLast().val;
+            first = (((LinkedList<TreeNode>) queue).peekFirst()).val;
+            last = (((LinkedList<TreeNode>) queue).peekLast()).val;
             while(size != 0){
                 TreeNode curr = queue.poll();
-                if(curr.left != null) queue.offer(curr.left);
-                if(curr.right != null) queue.offer(curr.right);
+                if(curr.left != null)queue.offer(curr.left);
+                if(curr.right != null)queue.offer(curr.right);
                 size--;
             }
         }
-        return new int[]{first, last};
+        return new DeepestNodes(first,last);
     }
-
     public TreeNode lowestCommonAncestor(TreeNode root, int p, int q) {
-        if(root == null)
-            return null;
-        if(root.val == p || root.val == q)
-            return root;
-
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-
-        if(left != null && right != null)
-            return root;
-        return (left != null) ? left : right;
+    if(root == null)
+        return null;
+    if(root.val == p|| root.val == q)
+        return root;
+    
+    TreeNode left = lowestCommonAncestor(root.left, p, q);
+    TreeNode right = lowestCommonAncestor(root.right,p,q);
+    if(left == null && right ==null)
+        return null;
+    if(left != null && right != null){
+        return root;
     }
+    if(left!= null)
+        return left;
+    else
+        return right;
+}
+
 }
