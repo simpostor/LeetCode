35a36bc0dc8828a5092f3747cfe318450a6549da
Atharva Sankhe <84955573+simpostor@users.noreply.github.com>
2025-04-14 13:43:39 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (13.22%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,7 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int calculated = Arrays.stream(nums).sum();
+        int calculated = 0;
+        for(int num : nums)
+            calculated += num;
         //expected sum should be 
         int expected = (n*(n+1))/2;
         return expected == calculated ? 0 : (expected - calculated);
