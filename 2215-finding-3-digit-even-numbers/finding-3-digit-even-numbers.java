342c2c5c2c439c2d0b168571b193a45393c14fa3
Atharva Sankhe <84955573+simpostor@users.noreply.github.com>
2025-05-12 15:22:28 +0530

    Time: 1214 ms (5.11%) | Memory: 45.6 MB (10.22%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
@@ -1,30 +1,25 @@
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
-        List<Integer> list = new ArrayList<>();
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int digit : digits)map.put(digit,map.getOrDefault(digit,0)+1);
-        for(int i = 100; i < 999; i++){
-            if(i % 2 != 0)continue;
-            if(isValid(i,map))list.add(i);
-        }
-        int n = list.size();
-        int[] res = new int[n];
+        Set<Integer> set = new HashSet<>();
+        int n = digits.length;
         for(int i = 0; i < n; i++){
-            res[i] = list.get(i);
+            if(digits[i] == 0)continue;
+            for(int j = 0; j < n; j++){
+                if(j==i)continue;
+                for(int k = 0; k < n; k++){
+                    if(k==i||k==j||digits[k]%2!=0)continue;
+                    String num = digits[i]+""+digits[j]+""+digits[k];
+                    set.add(Integer.parseInt(num));
+                }
+            }
         }
+        n = set.size();
+        int[] res = new int[n];
+        int i = 0;
+        for(int num : set){
+            res[i] = num;i++;
+        }
+        Arrays.sort(res);
         return res;
-    }
-    public boolean isValid(int num, Map<Integer,Integer> map){
-        Map<Integer,Integer> nummap = new HashMap<>();
-        while(num != 0){
-            int digit = num%10;
-            nummap.put(digit,nummap.getOrDefault(digit,0)+1);
-            num /= 10;
-        }
-        for(int n : nummap.keySet()){
-            int freq = nummap.get(n);
-            if((!map.containsKey(n)) || (freq > map.get(n)))return false;
-        }
-        return true;//if we go through for and dont get false then its true
     }
 }
