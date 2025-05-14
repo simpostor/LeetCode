9d3e8efcfa39c67e164b161954e65e7ae247f819
Atharva Sankhe <84955573+simpostor@users.noreply.github.com>
2025-05-14 14:48:54 +0530

    Time: 2 ms (99.04%) | Memory: 43.1 MB (69.61%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int l = 0;
-        int r = s.length()-1;
+        int l = 0; int r = s.length()-1;
+        if(s.length() == 1)return true;
         while(l<r){
-            while(l<r && !Character.isLetterOrDigit(s.charAt(l))){//while l is not letter or digit
-                l++;
-            }//now l at an index where char is letter or digit
-            while(l<r && !Character.isLetterOrDigit(s.charAt(r))){//while l is not letter or digit
-                r--;
-            }// s at r has letter or digit
-            if(Character.toUpperCase(s.charAt(r)) != Character.toUpperCase(s.charAt(l)))
+            //skip till valid l
+            //skip till valid r
+            //compare and return false if not same
+            while(l<r && !Character.isLetterOrDigit(s.charAt(l))) l++;
+            while(l<r && !Character.isLetterOrDigit(s.charAt(r))) r--;
+
+            char charl = s.charAt(l);
+            char charr = s.charAt(r);
+            if(Character.toLowerCase(charr) != Character.toLowerCase(charl))
                 return false;
-            l++;
-            r--;
+            l++;r--;
         }
         return true;
     }
