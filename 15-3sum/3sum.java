0f7ca88f2729064498513ffc764d3e607224ef0a
Atharva Sankhe <84955573+simpostor@users.noreply.github.com>
2025-05-14 15:07:56 +0530

    Time: 28 ms (80.16%) | Memory: 51.2 MB (96.60%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -3,25 +3,25 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
         for(int i = 0; i < nums.length; i++){
-            if(i>0 && nums[i] == nums[i-1])
-                continue;
+            //skip i if same
+            if(i>0 && nums[i] == nums[i-1]) continue;
             int l = i + 1;
             int r = nums.length-1;
             while(l<r){
-                int sum = nums[i] + nums[l] + nums[r];
-                if(sum > 0)
-                    r--;
-                else if(sum < 0)
-                    l++;
+                int sum = nums[i]+nums[l]+nums[r];
+                //if sum greater than target then to decrease it we do r--
+                //if sum smaller than target to inc we do l++
+                //else we add in l
+                if(sum>0)r--;
+                else if(sum<0)l++;
                 else{
-                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                    l++;
-                    r--;
-                    while(l<r && nums[l] == nums[l-1])
-                        l++;
-                    while(l<r && nums[r] == nums[r+1])
-                        r--;
-                    
+                    // add in res
+                    res.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    //now skip l and r till im getting new
+                    l++;r--;
+                    while(l<r && nums[l]==nums[l-1])l++;
+                    while(l<r && nums[r] == nums[r+1])r--;
+
                 }
             }
         }
